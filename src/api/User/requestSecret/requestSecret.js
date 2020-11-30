472f4e46ba5c46f26f34206dde1caa280570f771
gangslee <grandnet1225@gmail.com>
2020-11-30 12:01:48 +0900

    Passport JWT part Three

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -3,11 +3,13 @@ import { generateSecret, sendSecretMail } from '../../../utils';
 
 export default {
   Mutation: {
-    requestSecret: async (_, args) => {
+    requestSecret: async (_, args, { request }) => {
+      console.log(request);
       const { email } = args;
       const loginSecret = generateSecret();
 
       try {
+        throw Error();
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
