ed2693a1e07894ef2a97df0ec40a85dbf57fc33c
gangslee <grandnet1225@gmail.com>
2020-12-11 23:56:44 +0900

    seeFullPost Refactoring

--- src/fragments.js
+++ src/fragments.js
@@ -3,6 +3,44 @@ export const COMMENT_FRAGMENT = `
         id
         text
         user{
+            ${USER_FRAGMENT}
+        }
+    }
+`;
+
+export const USER_FRAGMENT = `
+    fragment UserParts on User{
+        id
+        userName
+    }
+`;
+
+export const FILE_FRAGMENT = `
+    fragment FileParts on File{
+        id
+        url
+    }
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post{
+        id
+        caption
+        location
+        files{
+            id
+            url
+        }
+        comments{
+            id
+            text
+            user{
+                id
+                userName
+            }
+        }
+        user{
+            id
             userName
         }
     }
