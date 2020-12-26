7a2fe2631486a84b9ac549a10036a30bbdfc8015
gangslee <grandnet1225@gmail.com>
2020-12-26 18:41:03 +0900

    Fix createAccount

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,11 +1,19 @@
-import { prisma } from "../../../../generated/prisma-client";
+import { prisma } from '../../../../generated/prisma-client';
 
 export default {
   Mutation: {
     createAccount: async (_, args) => {
-      const { userName, email, firstName = "", lastName = "", bio = "" } = args;
+      const { userName, email, firstName = '', lastName = '', bio = '' } = args;
+      const exists = await prisma.$exists.user({
+        OR: [{ userName }, { email }],
+      });
 
-      await prisma.createUser({ userName, email, firstName, lastName, bio });
+      if (exists) {
+        throw Error('This username / email is already taken');
+      }
+
+      const data = await prisma.createUser({ userName, email, firstName, lastName, bio });
+      console.log(data);
       return true;
     },
   },
