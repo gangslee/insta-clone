87d3e1880bb1cab0b0c47683a19f23912ad5b1ea
gangslee <grandnet1225@gmail.com>
2021-01-04 01:25:43 +0900

    Fix addComment

--- src/api/Comment/addComment/addComment.js
+++ src/api/Comment/addComment/addComment.js
@@ -1,14 +1,13 @@
-import { prisma } from '../../../../generated/prisma-client';
 import { isAuthenticated } from '../../../middlewares';
+import { prisma } from '../../../../generated/prisma-client';
 
 export default {
   Mutation: {
     addComment: async (_, args, { request }) => {
       isAuthenticated(request);
-
       const { text, postId } = args;
       const { user } = request;
-      const comment = await prisma.createComment({
+      return prisma.createComment({
         user: {
           connect: {
             id: user.id,
@@ -21,7 +20,6 @@ export default {
         },
         text,
       });
-      return comment;
     },
   },
 };
